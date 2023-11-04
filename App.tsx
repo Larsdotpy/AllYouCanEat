47009506db3313194505ed156e406edb6716cb0c
LarsDotPy <larsdebruijn8@gmail.com>
2023-11-04 14:56:48 +0100

    updated order screen

--- App.tsx
+++ App.tsx
@@ -56,9 +56,9 @@ const FoodScreen: React.FC = () => {
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const OrdersScreen: React.FC = () => {
+const OrdersScreen = () => {
 
-  const OrderItem: React.FC<{ orderDetails: string }> = ({ orderDetails }) => {
+  const OrderItem = ({ orderDetails }) => {
     return (
       <View style={styles.orderItem}>
         <Text>{orderDetails}</Text>
@@ -67,21 +67,44 @@ const OrdersScreen: React.FC = () => {
     );
   };
 
-  
-  // Assuming orders data is available in an array (replace this with your actual data)
   const orders = [
     "Order 1 details",
     "Order 2 details",
     "Order 3 details",
+    "Order 4 details",
+    "Order 5 details",
+    "Order 6 details",
+    "Order 7 details",
+    "Order 8 details",
+    "Order 9 details",
+    "Order 10 details",
+    "Order 11 details",
+    "Order 12 details",
+    "Order 13 details",
+    "Order 14 details",
+    "Order 15 details",
     // Add more orders or retrieve from your data source
   ];
+
+  const placeOrderHandler = () => {
+    // Functionality to handle placing an order
+    // Add your logic here
+    console.log('Placing the order...');
+  };
 
   return (
     <View style={styles.ordersContainer}>
       <Text style={styles.screenTitle}>Orders Made in Current Round</Text>
-      {orders.map((order, index) => (
-        <OrderItem key={index} orderDetails={order} />
-      ))}
+      <FlatList
+        data={orders}
+        renderItem={({ item }) => <OrderItem orderDetails={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.flatListContainer}
+      />
+
+      <TouchableOpacity style={styles.placeOrderButton} onPress={placeOrderHandler}>
+        <Text style={styles.placeOrderText}>Place Order</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -489,6 +512,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   orderDetails: {
+    fontSize: 16,
+  },
+  placeOrderButton: {
+    // Styles for the "Place Order" button
+    // Center the button and style it accordingly
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue', // Example background color
+    padding: 10,
+    marginTop: 20, // Adjust the margin based on your design
+  },
+  placeOrderText: {
+    // Styles for the text inside the "Place Order" button
+    color: 'white',
     fontSize: 16,
   },
   totalContainer: {
